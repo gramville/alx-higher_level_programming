d9e0df0b4008d82eb6f8e7748cdc50d5fe5b871b
gramville <111007717+gramville@users.noreply.github.com>
2023-01-02 19:30:50 +0300

    Add files via upload

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""The "add_integer" module
+that supplies one function, add_integer"""
+
+def add_integer(a, b=98):
+    """a function that adds 2 integers and 
+    returns the result"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return (a + b)
